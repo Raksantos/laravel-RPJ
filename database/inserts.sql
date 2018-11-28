565e619a901fda3853fb73ef83e177b8863c4b0d
Joao Falcao <joao.falcao@icloud.com>
2018-11-28 18:49:05 -0300

    "use" adicionado nos scripts do banco

--- database/inserts.sql
+++ database/inserts.sql
@@ -1,3 +1,5 @@
+USE db_rpj;
+
 INSERT INTO t_produto (nome, preco, marca) VALUES ('PC Gamer Inspiron', 3449, 'Dell');
 INSERT INTO t_produto (nome, preco, marca) VALUES ('RTX 2080', 4700, 'Nvidia');
 INSERT INTO t_produto (nome, preco, marca) VALUES ('iPhone XS', 5000, 'Apple');
