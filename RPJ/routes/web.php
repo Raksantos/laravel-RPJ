2bf4b4e8d0a611764909e563fe1ecb8a4705278e
Joao Falcao <joao.falcao@icloud.com>
2018-11-28 21:04:47 -0300

    tentando usar o template e olha no que deu

--- RPJ/routes/web.php
+++ RPJ/routes/web.php
@@ -4,4 +4,7 @@ Route::get('/', function(){
     return '<h1>RPJ - Nem existe direito ainda</h1>';
 });
 
+//falha miserável de usar o template
+//Route::get('/', 'ProdutosController@home');
+
 Route::get('/produtos', 'ProdutosController@listarProdutos');
