959c7d68af274a69a89b0d93d9e7ae8108a72e43
Joao Falcao <joao.falcao@icloud.com>
2018-12-18 16:07:44 -0300

    view com "coleções"

--- RPJ/resources/views/home.blade.php
+++ RPJ/resources/views/home.blade.php
@@ -9,31 +9,7 @@
 
 	<title>RPJ Tecnologia</title>
 
-	<!-- Google font -->
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
-
-	<!-- Bootstrap -->
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
-
-	<!-- Slick -->
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/slick.css') }}" />
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/slick-theme.css') }}" />
-
-	<!-- nouislider -->
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/nouislider.min.css') }}" />
-
-	<!-- Font Awesome Icon -->
-	<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
-
-	<!-- Custom stlylesheet -->
-	<link type="text/css" rel="stylesheet" href="{{ asset('css/style.css') }}" />
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js')"></script>
-		<![endif]-->
+	@include('imports-css')
 
 </head>
 
@@ -42,59 +18,7 @@
 
 	@include('nav')
 
-	<!-- SECTION -->
-	<div class="section">
-		<!-- container -->
-		<div class="container">
-			<!-- row -->
-			<div class="row">
-				<!-- shop -->
-				<div class="col-md-4 col-xs-6">
-					<div class="shop">
-						<div class="shop-img">
-							<img src="./img/shop01.png" alt="">
-						</div>
-						<div class="shop-body">
-							<h3>Laptop<br>Collection</h3>
-							<a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
-						</div>
-					</div>
-				</div>
-				<!-- /shop -->
-
-				<!-- shop -->
-				<div class="col-md-4 col-xs-6">
-					<div class="shop">
-						<div class="shop-img">
-							<img src="./img/shop03.png" alt="">
-						</div>
-						<div class="shop-body">
-							<h3>Accessories<br>Collection</h3>
-							<a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
-						</div>
-					</div>
-				</div>
-				<!-- /shop -->
-
-				<!-- shop -->
-				<div class="col-md-4 col-xs-6">
-					<div class="shop">
-						<div class="shop-img">
-							<img src="./img/shop02.png" alt="">
-						</div>
-						<div class="shop-body">
-							<h3>Cameras<br>Collection</h3>
-							<a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
-						</div>
-					</div>
-				</div>
-				<!-- /shop -->
-			</div>
-			<!-- /row -->
-		</div>
-		<!-- /container -->
-	</div>
-	<!-- /SECTION -->
+	@include('colecoes')
 
 	<!-- SECTION -->
 	<div class="section">
@@ -160,13 +84,7 @@
 
 	@include('footer')
 
-	<!-- jQuery Plugins -->
-	<script src="{{ asset('js/jquery.min.js') }}"></script>
-	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-	<script src="{{ asset('js/slick.min.js') }}"></script>
-	<script src="{{ asset('js/nouislider.min.js') }}"></script>
-	<script src="{{ asset('js/jquery.zoom.min.js') }}"></script>
-	<script src="{{ asset('js/main.js') }}"></script>
+	@include('imports-js')
 
 </body>
 
