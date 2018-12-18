5b67020564177f3934d234582901244201fdf508
Joao Falcao <joao.falcao@icloud.com>
2018-12-18 16:04:21 -0300

    views só com imports de js e css

--- RPJ/resources/views/produto.blade.php
+++ RPJ/resources/views/produto.blade.php
@@ -8,31 +8,7 @@
 
 		<title>RPJ Tecnologia</title>
 
- 		<!-- Google font -->
- 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
-
- 		<!-- Bootstrap -->
- 		<link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"/>
-
- 		<!-- Slick -->
- 		<link type="text/css" rel="stylesheet" href="{{ asset('css/slick.css') }}"/>
- 		<link type="text/css" rel="stylesheet" href="{{ asset('css/slick-theme.css') }}"/>
-
- 		<!-- nouislider -->
- 		<link type="text/css" rel="stylesheet" href="{{ asset('css/nouislider.min.css') }}"/>
-
- 		<!-- Font Awesome Icon -->
- 		<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
-
- 		<!-- Custom stlylesheet -->
- 		<link type="text/css" rel="stylesheet" href="{{ asset('css/style.css') }}"/>
-
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+ 		@include('imports-css')
 
     </head>
 	<body>
@@ -145,13 +121,7 @@
 
         @include('footer')
 
-		<!-- jQuery Plugins -->
-		<script src="{{ asset('js/jquery.min.js') }}"></script>
-		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-		<script src="{{ asset('js/slick.min.js') }}"></script>
-		<script src="{{ asset('js/nouislider.min.js') }}"></script>
-		<script src="{{ asset('js/jquery.zoom.min.js') }}"></script>
-		<script src="{{ asset('js/main.js') }}"></script>
+		@include('imports-js')
 
 	</body>
 </html>
